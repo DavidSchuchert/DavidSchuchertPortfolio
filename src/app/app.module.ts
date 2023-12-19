daea13568bbeef4b1dcbbf7c95af1d42d2e7ea36
DavidSchuchert <Davidschuchert@icloud.com>
2023-12-19 16:35:03 +0100

    added social section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { IAMComponent } from './iam/iam.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    IAMComponent
+    IAMComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
