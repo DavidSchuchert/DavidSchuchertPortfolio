af73a4c62522691d2a8dcba5e812fe41bc5e993f
DavidSchuchert <Davidschuchert@icloud.com>
2024-01-02 16:24:06 +0100

    added mail sucessfull on Contactfield message

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,85 +1,87 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { SharedService } from '../shared.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrl: './contact.component.scss',
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-  @ViewChild('myForm')
-  myForm!: ElementRef;
+  // ViewChild properties to access DOM elements
+  @ViewChild('myForm') myForm!: ElementRef<HTMLFormElement>;
+  @ViewChild('nameField') nameField!: ElementRef<HTMLInputElement>;
+  @ViewChild('mailField') mailField!: ElementRef<HTMLInputElement>;
+  @ViewChild('messageField') messageField!: ElementRef<HTMLTextAreaElement>;
+  @ViewChild('sendButton') sendButton!: ElementRef<HTMLButtonElement>;
 
-  @ViewChild('nameField')
-  nameField!: ElementRef;
-
-  @ViewChild('messageField')
-  messageField!: ElementRef;
-
-  @ViewChild('sendButton')
-  sendButton!: ElementRef;
-
-  @ViewChild('mailField')
-  mailField!: ElementRef;
-
+  // Properties to track privacy policy consent and the display of the success message
   showPrivacyPolicy: boolean = false;
+  showSuccessMessage: boolean = false;
 
+  // Injecting the SharedService to manage shared states like the privacy policy consent
   constructor(private sharedService: SharedService) {
-
-    // Subscribe to the privacy policy state
-    this.sharedService.showPrivacyPolicy.subscribe(value => {
+    this.sharedService.showPrivacyPolicy.subscribe((value) => {
       this.showPrivacyPolicy = value;
     });
   }
 
+  // Function to handle form submission
   async sendMail() {
-    let nameField = this.nameField.nativeElement;
-    let mailField = this.mailField.nativeElement;
-    let messageField = this.messageField.nativeElement;
-    let sendButton = this.sendButton.nativeElement;
-    nameField.disabled = true;
-    mailField.disabled = true;
-    messageField.disabled = true;
-    sendButton.disabled = true;
+    this.toggleFieldsDisabledState(true);
+    await this.submitForm();
+    this.clearForm();
+    this.displaySuccessMessage();
+  }
+  // Function to enable or disable form fields and the submit button
+  private toggleFieldsDisabledState(isDisabled: boolean) {
+    this.nameField.nativeElement.disabled = isDisabled;
+    this.mailField.nativeElement.disabled = isDisabled;
+    this.messageField.nativeElement.disabled = isDisabled;
+    this.sendButton.nativeElement.disabled = isDisabled;
+  }
 
-    let fd = new FormData();
-    fd.append('name', nameField.value);
-    fd.append('mail', mailField.value);
-    fd.append('message', messageField.value);
-    //send
+  // Function to submit form data to the server
+  private async submitForm() {
+    const formData = new FormData();
+    formData.append('name', this.nameField.nativeElement.value);
+    formData.append('mail', this.mailField.nativeElement.value);
+    formData.append('message', this.messageField.nativeElement.value);
+
     await fetch('https://david-schuchert.de/send_mail/send_mail.php', {
       method: 'POST',
-      body: fd,
+      body: formData,
     });
-
-    nameField.disabled = false;
-    mailField.disabled = false;
-    messageField.disabled = false;
-    sendButton.disabled = false;
   }
 
+  // Function to clear form fields and re-enable them after submission
+  private clearForm() {
+    this.nameField.nativeElement.value = '';
+    this.mailField.nativeElement.value = '';
+    this.messageField.nativeElement.value = '';
+    this.toggleFieldsDisabledState(false);
+  }
+
+  // Function to display a success message for a brief period
+  private displaySuccessMessage() {
+    this.showSuccessMessage = true;
+    setTimeout(() => {
+      this.showSuccessMessage = false;
+    }, 3000);
+  }
+
+  // Function to toggle the disabled state of the send button based on checkbox
   checkboxCheck() {
-    let sendButton = this.sendButton.nativeElement;
-
-
-    if (sendButton.disabled == true) {
-      sendButton.disabled = false;
-    } else {
-      sendButton.disabled = true;
-    }
+    this.sendButton.nativeElement.disabled =
+      !this.sendButton.nativeElement.disabled;
   }
 
-scrollTop(){
-  window.scroll({
-    top: 0,
-    left: 0,
-    behavior: 'smooth'
-  });
-}
+  // Function to scroll the window to the top smoothly
+  scrollTop() {
+    window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+  }
 
-openPrivacyPolicy() {
-  this.sharedService.togglePrivacyPolicy();
-}
-
-
+  // Function to open or close the privacy policy
+  openPrivacyPolicy() {
+    this.sharedService.togglePrivacyPolicy();
+  }
 }
